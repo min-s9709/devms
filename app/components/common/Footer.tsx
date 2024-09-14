5dd21ceae2549130b3608b0eb8c91d8645fd3475
min-s9709 <mim970929@naver.com>
2024-09-14 18:48:23 +0900

    feat: Header, Footer 공통 컴포넌트

---
+++ app/components/common/Footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className='flex items-center justify-center w-full bg-gray-100 h-14'>
+      <div className='w-4/5 text-center'>
+        <p className='text-dark-gray-color'>
+          Copyright © @min-s9709. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  )
+}
---
+++ app/components/common/Header.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import { Icon } from '@iconify/react'
+import Link from 'next/link'
+import MenutabList from '@/components/common/MenutabList'
+import useMenuTabs from '../hooks/useMenuTabs'
+
+export default function Header() {
+  const { isMenuOpen, handleMenuClick, menuRef } = useMenuTabs()
+  return (
+    <header
+      className={
+        'fixed z-10 w-full h-16 bg-white flex items-center justify-center'
+      }
+    >
+      <div className={'w-[1260px] flex'}>
+        <div className={'flex w-3/4 items-center pl-2'}>
+          <Link href='/'>
+            <h1 className={'text-2xl font-bold'}>DEV.MS</h1>
+          </Link>
+        </div>
+        <div className={'flex w-1/4 justify-end'}>
+          <div
+            className={
+              'flex w-32 justfiy-end border-l-2 justify-around items-center'
+            }
+          >
+            <div ref={menuRef} className={'relative'}>
+              <div
+                className={
+                  'w-6 h-6 hover:bg-[#EEEFF1] hover:rounded-full cursor-pointer'
+                }
+                onClick={handleMenuClick}
+              >
+                {isMenuOpen ? (
+                  <Icon icon='mingcute:close-line' className={'w-6 h-6'} />
+                ) : (
+                  <Icon icon='mingcute:menu-line' className={'w-6 h-6'} />
+                )}
+              </div>
+              {isMenuOpen && <MenutabList />}
+            </div>
+
+            <Icon
+              icon='material-symbols:dark-mode-outline'
+              className={'w-6 h-6'}
+            />
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
