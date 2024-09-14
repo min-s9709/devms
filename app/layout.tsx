8133242a2333670a075dd92551c93609b7632aa0
min-s9709 <mim970929@naver.com>
2024-09-14 18:08:36 +0900

    style: 전역 폰트 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,26 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next'
+import './globals.css'
+import localFont from 'next/font/local'
+
+const pretendard = localFont({
+  src: './fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+})
 
 export const metadata: Metadata = {
-  title: "DEV-MS",
-  description: "프론트엔득 개발자 김민성의 개인 플랫폼입니다.",
-};
+  title: 'DEV-MS',
+  description: '프론트엔득 개발자 김민성의 개인 플랫폼입니다.',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="ko">
-      <body>{children}</body>
+    <html lang='ko'>
+      <body className={pretendard.className}>{children}</body>
     </html>
-  );
+  )
 }
