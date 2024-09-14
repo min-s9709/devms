e2f62bf583860fc9389da2d3d30b9d54053815b4
min-s9709 <mim970929@naver.com>
2024-09-14 18:08:08 +0900

    style: theme 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,28 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      fontSize: {
+        'size-big-title': '1.5rem',
+        'size-medium-title': '1.125rem',
+        'size-body': '1rem',
+        'size-subbody': '0.75rem',
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'dark-gray-color': '#777777',
+        'medium-gray-color': '#00000080',
+        'light-gray-color': '#E5E5E5',
+        'c-green-color': '#12B886',
+        'c-light-color': '#F8F9FA',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
