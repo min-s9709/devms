738f83330dcf712af021380a3545257f969eb89c
min-s9709 <mim970929@naver.com>
2024-09-16 16:29:40 +0900

    add: data 추가

---
+++ app/data/introduce.ts
@@ -0,0 +1,31 @@
+export const aboutData = {
+  tag: ['#프론트엔드', '소통', '협업'],
+  introduce: [
+    {
+      question: '흐음,,, 어떤 내용이 들어가야할까 ',
+      answer:
+        '사용자 중심 디자인, 협업, 프론트엔드 기초 자삭울 쎃았습니다. 사용자와의 소통에 대해 중요성을 인지하고, 더 밀접하게 상호작용 가능한 프로젝트를 개발하고자 ',
+    },
+    {
+      question: '흐음,,, 어떤 내용이 들어가야할까 ',
+      answer:
+        '사용자 중심 디자인, 협업, 프론트엔드 기초 자삭울 쎃았습니다. 사용자와의 소통에 대해 중요성을 인지하고, 더 밀접하게 상호작용 가능한 프로젝트를 개발하고자 ',
+    },
+  ],
+  stacks: {
+    skills: [
+      { src: '/images/stacks/HTML.svg', name: 'html' },
+      { src: '/images/stacks/CSS.svg', name: 'css' },
+      { src: '/images/stacks/JavaScript.svg', name: 'javascript' },
+      { src: '/images/stacks/TypeScript.svg', name: 'typescript' },
+      { src: '/images/stacks/React.svg', name: 'react' },
+      { src: '/images/stacks/TailwindCSS-Light.svg', name: 'tailwind-css' },
+      { src: '/images/stacks/StyledComponents.svg', name: 'styled-components' },
+    ],
+    tools: [
+      { src: '/images/stacks/Github-Light.svg', name: 'github' },
+      { src: '/images/stacks/VSCode.svg', name: 'vscode' },
+      { src: '/images/stacks/Discord.svg', name: 'discord' },
+    ],
+  },
+}
---
+++ app/data/projects.ts
@@ -0,0 +1,22 @@
+export const projectData = [
+  {
+    title: 'DEV.MS',
+    content: '프론트엔드 개발자 김민성을 보여주는 개인 플랫폼입니다.',
+    tag: ['#포트폴리오', '#블로그', '#이력서'],
+    img: '/images/homeprofile.png',
+  },
+  {
+    title: 'ON&OFF',
+    content:
+      '사람들이 서로 만남을 갖고 소통하며 상호 협력과 경쟁을 통해 성장할 수 있는 기회를 제공하기 위한 서비스입니다.',
+    tag: ['#팀', '#모임', '#관심사'],
+    img: '/images/project/onandoff.png',
+  },
+  {
+    title: 'Elice-Motors',
+    content:
+      '딱딱하게 생각할 수 있는 자동차를 편리하고 효율적으로 구매 및 비교 관리할 수 있도록 돕는 플랫폼 서비스입니다.',
+    tag: ['#팀', '#자동차', '#쇼핑몰'],
+    img: '/images/project/elicemotors.png',
+  },
+]
