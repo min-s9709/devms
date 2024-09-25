5ab1b4962e092f17dc36dd4ff76162d981f35dab
min-s9709 <mim970929@naver.com>
2024-09-25 17:03:00 +0900

    feat: post 관련 라이브러리 함수 추가 및 수정

--- app/lib/post.ts
+++ app/lib/post.ts
@@ -1,6 +1,8 @@
+import { Post } from '@/type'
 import fs from 'fs'
 import { sync } from 'glob'
 import matter from 'gray-matter'
+import { formatDate } from '@/lib/util'
 
 const postsDirectory = 'posts'
 
@@ -9,10 +11,19 @@ export const getAllPostsPath = () => {
   return paths
 }
 
-export const parsePost = (postPath: string) => {
+export const parsePost = async (postPath: string): Promise<Post> => {
   const file = fs.readFileSync(postPath, 'utf8')
   const { data, content } = matter(file)
-  return { data, content }
+  return {
+    data: {
+      title: data.title,
+      category: data.category,
+      date: data.date,
+      headerImg: data.headerImg,
+      slug: data.slug,
+    },
+    content,
+  }
 }
 
 export const getAllPosts = async () => {
@@ -20,15 +31,25 @@ export const getAllPosts = async () => {
   const allPostsData = await Promise.all(
     paths.map((postPath) => parsePost(postPath)),
   )
-  return allPostsData
+  const sortedAllPosts = allPostsData.sort(
+    (a, b) => new Date(b.data.date).getTime() - new Date(a.data.date).getTime(),
+  )
+  return sortedAllPosts
+}
+
+export const getPostDetail = async (postPath: string) => {
+  const post = await parsePost(`posts/${postPath}/${postPath}.mdx`)
+  const { data, content } = post
+  const date = formatDate(data.date)
+  return { ...data, date, content }
 }
 
 export const getCategoryPosts = async (category: string) => {
   const allPostsData = await getAllPosts()
-  const filteredPosts = allPostsData.filter(
+  const filteredAllPosts = allPostsData.filter(
     (post) => post.data.category === category,
   )
-  return filteredPosts
+  return filteredAllPosts
 }
 
 export const getAllCategories = async () => {
@@ -37,4 +58,24 @@ export const getAllCategories = async () => {
   const distinctCategoryList = [...new Set(categoryList)]
 
   return distinctCategoryList
+}
+
+export const getPreviousNext = async (postPath: string) => {
+  const allPosts = await getAllPosts()
+  const targetIndex = allPosts.findIndex((post) => post.data.slug === postPath)
+  const previous =
+    targetIndex === allPosts.length - 1 ? null : allPosts[targetIndex + 1]
+
+  const next = targetIndex === 0 ? null : allPosts[targetIndex - 1]
+
+  return {
+    previous: {
+      title: previous?.data.title,
+      slug: previous?.data.slug,
+    },
+    next: {
+      title: next?.data.title,
+      slug: next?.data.slug,
+    },
+  }
 }
