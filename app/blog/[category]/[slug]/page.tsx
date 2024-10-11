e759179735af0a27aebb76a44577efd9548fd8f6
min-s9709 <mim970929@naver.com>
2024-10-11 19:50:47 +0900

    feat: 블로그 상세 페이지toc 추가

--- app/blog/[category]/[slug]/page.tsx
+++ app/blog/[category]/[slug]/page.tsx
@@ -1,9 +1,10 @@
-import { getPostDetail, getPreviousNext } from '@/lib/post'
+import { getPostDetail, getPreviousNext, getToc } from '@/lib/post'
 import Section from '@/components/blog/section/Section'
 import PostHeader from '@/components/blog/posts/PostHeader'
 import PostBody from '@/components/blog/posts/PostBody'
 import PostProfile from '@/components/blog/posts/PostProfile'
 import PostNavigator from '@/components/blog/posts/PostNavigator'
+import TableOfContent from '@/components/blog/posts/TableOfContent'
 
 interface IPostDetailPageProps {
   params: {
@@ -18,11 +19,14 @@ export default async function PostDetail({
   const post = await getPostDetail(slug)
   const { previous, next } = await getPreviousNext(slug)
   const { title, category, date, content } = post
-
+  const toc = getToc(content)
   return (
-    <Section>
+    <Section type='small'>
       <PostHeader title={title} category={category} date={date} />
-      <PostBody content={content} />
+      <article className='relative'>
+        <TableOfContent toc={toc} />
+        <PostBody content={content} />
+      </article>
       <PostProfile />
       <PostNavigator previous={previous} next={next} category={category} />
     </Section>
