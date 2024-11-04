89f57026d7294bfe52c83e194bfb30a303375cd1
min-s9709 <mim970929@naver.com>
2024-11-04 16:30:24 +0900

    add: id 추가 및 padding 추가

--- app/components/home/section/Section.tsx
+++ app/components/home/section/Section.tsx
@@ -1,15 +1,21 @@
 interface ISectionProps {
   type?: 'common' | 'main'
   children: React.ReactNode
+  id?: string
 }
 
-export default function Section({ children, type = 'common' }: ISectionProps) {
+export default function Section({
+  children,
+  type = 'common',
+  id,
+}: ISectionProps) {
   return (
     <section
+      id={id}
       className={
         type === 'main'
-          ? 'relative flex flex-col items-center justify-center w-full min-h-screen gap-8 px-4 mx-auto sm:px-6 md:px-8 border-b-[1px]'
-          : 'flex flex-col w-full justify-center max-w-screen-xl mx-auto min-h-screen gap-8 px-4 sm:px-6 md:px-8 border-b-[1px]'
+          ? 'relative flex flex-col items-center justify-center w-full min-h-screen py-6 gap-8 px-4 mx-auto sm:px-6 md:px-8 border-b-[1px]'
+          : 'flex flex-col w-full justify-center max-w-screen-xl py-6 mx-auto min-h-screen gap-8 px-4 sm:px-6 md:px-8 border-b-[1px]'
       }
     >
       {children}
